e5bd87ff9032e005185cdc2f91f8aa7b21d4d372
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-08-06 10:23:49 +0800

    开启功能

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
@@ -15,16 +15,16 @@ object ModelOrder {
         BaseModel::class.java,       // 基础设置
         AntForest::class.java,       // 森林
         AntFarm::class.java,         // 庄园
-//        AntOrchard::class.java,    // 农场
+        AntOrchard::class.java,      // 农场
         AntOcean::class.java,        // 海洋
-        AntDodo::class.java,       // 神奇物种
+        AntDodo::class.java,         // 神奇物种
         AncientTree::class.java,     // 古树
         AntCooperate::class.java,    // 合种
-        Reserve::class.java,       // 保护地
+        Reserve::class.java,         // 保护地
         AntSports::class.java,       // 运动
-//        AntMember::class.java,     // 会员
-//        AntStall::class.java,      // 蚂蚁新村
-//        GreenFinance::class.java,  // 绿色经营
+        AntMember::class.java,       // 会员
+        AntStall::class.java,        // 蚂蚁新村
+        GreenFinance::class.java,    // 绿色经营
 //        AntBookRead::class.java,   // 读书
 //        ConsumeGold::class.java,   // 消费金
 //        OmegakoiTown::class.java,  // 小镇
